ec88d16e0d1b193ad6b069f01ade2ac89c2fc8a4
Marcos Meneses <marcosvnm1@hotmail.com>
2025-05-15 15:05:46 -0300

    :tada:Commit Inicial - Criado Hello World

---
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+	@GetMapping
+	public String hello() {
+		return "<b>Hello World!</b>";
+	}
+	
+	@GetMapping("/turma82")
+	public String turmaJava82() {
+		return "<b>A Turma Java 82 é TOP!</b>";
+	}
+	
+}
